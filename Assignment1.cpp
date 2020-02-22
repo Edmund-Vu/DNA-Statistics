dba51998468e4ddc15eabf65380e70e89783f1d5
Edmund <edmundvu816@gmail.com>
2020-02-22 23:41:58 +0000

    Bugfixing - fixed variable names, local scope declarations, mistypes

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -26,37 +26,40 @@ dnaStats::~dnaStats(){
 }
 
 void readFromFile(string input){
+  int lineCount = 0;
+  string dnaString = "";
   ifstream inFile;
   inFile.open(input);
   char c;
 
-  while(!inStream.eof()){
+  while(!inFile.eof()){
     if(c == '\n'){ //checks whether the read character is a newline
       lineCount++; //tracks total new lines in the file
-    } else{
-    inStream.get(c);
+    }
+    inFile.get(c);
     dnaString += c;
   }
 }
 
 void dnaStats::calcStats(){
-  int countLine; // tracks the number of lines read
+  int countLine = 0;; // tracks the number of lines read
 
   for(int i = 0; i < dnaString.size(); ++i){
     if(dnaString[i] != '\n'){
       nucleoTotal++;
     } else{
       countLine++;
-      squaredDiff += (nucleoTotal - mean)^2;
+      squaredDiff += pow((nucleoTotal - mean), 2);
       if(countLine == lineCount){
         sum = nucleoTotal;
         mean = sum / lineCount;
         variance = squaredDiff - nucleoTotal;
-        stDev = Sqrt(variance);
+        stDev = sqrt(variance);
       }
     }
   }
   cout << "The sum of the length of the DNA strings is: " << sum << endl;
   cout << "The mean of the length of the DNA strings is: " << mean << endl;
-  cout << "The variance of the length of the DNA strings is; " < variance << endl;
+  cout << "The variance of the length of the DNA strings is; " << variance << endl;
   cout << "The standard deviation of the length of the DNA strings is: " << stDev << endl;
+}
